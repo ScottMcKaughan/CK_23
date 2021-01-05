1f844ec1d4b8326fb4186cd4e010a9503c372346
Scott McKool <44004555+ScottMcKaughan@users.noreply.github.com>
2021-01-05 09:04:29 -0500

    redid rgbwooooyeah

--- src/main/java/frc/robot/subsystems/RGBWOOOOOOYEAHHH.java
+++ src/main/java/frc/robot/subsystems/RGBWOOOOOOYEAHHH.java
@@ -7,56 +7,94 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.PWM;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 //import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import frc.robot.Constants.RGBConstants;
 
 public class RGBWOOOOOOYEAHHH extends SubsystemBase {
+  // TODO: make more arguments to tailor how the rainbow looks and moves
+  m_led = new AddressableLED(// robot map led port here)
 
-  public PWM redLED;
-  public PWM blueLED;
-  public PWM greenLED;
-
-  //private SmartDashboard m_dash;
-  /**
-   * Creates a new RGBWOOOOOOYEAHHH.
-   */
   public RGBWOOOOOOYEAHHH() {
-    redLED = new PWM(RGBConstants.RED_LED);
-    blueLED = new PWM(RGBConstants.BLUE_LED);
-    greenLED = new PWM(RGBConstants.GREEN_LED);
+
   }
 
-  // private double IntToPWN(int input){
-  //   if(input > 255){ input = 255;}
-  //   if(input < 0){ input = 0;}
+    /**
+    * rainbow that moves down the LED strand at a constant rate
+    * sets the LED buffer to this info but does not change the LEDs
+    * do setData after this to update a strand of LEDs
+    */
+  privte void movingRainbow(int rate) {
+    for (var i = 0; i < m_ledBuffer.getLength(); i++) {
+
+      final var hue = (m_rainbowFirstPixelHue + (i * 180 / m_ledBuffer.getLength())) % 180;
+
+      m_ledBuffer.setHSV(i, hue, 255, 128);
+    }
+
+    m_rainbowFirstPixelHue += 3;
+
+    m_rainbowFirstPixelHue %= 180;
+  }
     
-  //   return input/255;
-  // }
+    /**
+    * rainbow that changes the whole strand, whole strand is the same color
+    * sets the LED buffer to this info but does not change the LEDs
+    * do setData after this to update a strand of LEDs
+    */
+  privte void rainbow() {
+    for (var i = 0; i < m_ledBuffer.getLength(); i++) {
 
-  /**
-   * 
-   * sets the rgb lights to a given red green blue values
-   * 
-   * @param red   red component 0-1
-   * @param green  blue component 0-1
-   * @param blue  green component 0-1
-   */
+      final var hue = (m_rainbowFirstPixelHue + (i * 180 / m_ledBuffer.getLength())) % 180;
 
-  public void SetRGBColor(int red, int green, int blue){
-    redLED.setRaw(Math.abs(red*255));
-    greenLED.setRaw(Math.abs(green*255));
-    blueLED.setRaw(Math.abs(blue*255));
+      m_ledBuffer.setHSV(i, hue, 255, 128);
+    }
 
-    // m_dash.putNumber("red", redLED.getRaw());
-    // m_dash.putNumber("green", greenLED.getRaw());
-    // m_dash.putNumber("blue", blueLED.getRaw());
+    m_rainbowFirstPixelHue += rate;
+
+    m_rainbowFirstPixelHue %= 180;
   }
+    
+    /**
+    * rainbow that moves the rainbow colors along with the robot's speed
+    * input the speed from the encoders
+    * sets the LED buffer to this info but does not change the LEDs
+    * do setData after this to update a strand of LEDs
+    */
+  privte void rainbowMoveWithRobot(int speed) { 
+    // TODO: add a correction value to turn speed into a number for sliding the rainbow
+    for (var i = 0; i < m_ledBuffer.getLength(); i++) {
 
+      final var hue = (m_rainbowFirstPixelHue + (i * 180 / m_ledBuffer.getLength())) % 180;
+
+      m_ledBuffer.setHSV(i, hue, 255, 128);
+    }
+
+    m_rainbowFirstPixelHue += speed; // good place for correction value
+
+    m_rainbowFirstPixelHue %= 180;
+  }
+    
+        /**
+    * rainbow that moves the rainbow colors along with the robot's speed
+    * input the speed from the encoders
+    * sets the LED buffer to this info but does not change the LEDs
+    * do setData after this to update a strand of LEDs
+    */
+  privte void solidColorToLEDs(int hue, int saturation, int value) { // pass through a color object or three ints? (3 ints for now)
+    
+    final var hue = // take color and convert it to hue
+    
+    for (var i = 0; i < m_ledBuffer.getLength(); i++) { 
+      m_ledBuffer.setHSV(i, hue, 255, 128);
+    }
+  }
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    rainbow(// put in speed number here);
+      
+    m_led.setData(m_ledBuffer);
   }
 }
